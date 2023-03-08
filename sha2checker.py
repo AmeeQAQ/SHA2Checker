8300664eb63edf14df997932906eabf6e4858cd9
AmeeQAQ <pablo.ortegah@um.es>
2023-03-08 16:53:14 +0100

    Modified help messages

--- sha2checker.py
+++ sha2checker.py
@@ -4,8 +4,8 @@ import argparse
 def parseargs():
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--function", help="Which SHA-2 function you need to hash your file with (sha224/sha256/sha384/sha512).", required=True)
-    parser.add_argument("-fi", "--file", help="The file you need to hash. Always provide the absolute route for the file unless this script is in the same directory as it", required=True)
-    parser.add_argument("-s", "--sum", help="Integrity hash value issued by the file owner", required=True)
+    parser.add_argument("-fi", "--file", help="file to be hashed. Use the absolute route if the file is not in the same directory as the script", required=True)
+    parser.add_argument("-s", "--sum", help="Hash value issued by the file owner", required=True)
     return parser
 
 def main():
